a8ba40b6e7e2ff111122f6ea474ba6a1a6e52e6a
ihainan <ihainan72@gmail.com>
2020-06-27 18:56:57 +0800

    Add v1 lighthouse support

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# Manager for V2 Lighthouses by Valve/HTC
-This python script helps you switch your Valve Lighthouses V2 on and into stand-by. Unfortunately, my Pimax 5K XR needs the lighthouses up and running before I turn it on, so the awesome work done by [@mann1x]( https://github.com/mann1x ) in his project [Pimax BS Manager](https://github.com/mann1x/pimax_bs_manager) is of little use to me. This may be different with your particular Pimax HMD.
+# Manager for V1 Lighthouses by Valve/HTC
+
+This project is based on [nouser2013](https://github.com/nouser2013)'s [lighthouse-v2-manager](https://github.com/nouser2013/lighthouse-v2-manager), thanks for the awesome work.
+
+This python script helps you switch your Valve Lighthouses v1 on and into stand-by. Unfortunately, my Pimax 5K XR needs the lighthouses up and running before I turn it on, so the awesome work done by [@mann1x]( https://github.com/mann1x ) in his project [Pimax BS Manager](https://github.com/mann1x/pimax_bs_manager) is of little use to me. This may be different with your particular Pimax HMD.
 
 ## installation (Windows binary release)
 1. Create a folder where you want to store the manager's files, e.g. `C:\Program Files\LH-Manager`. 
-1. Download and copy the files (executable and both icons) from [this repository's releases page](https://github.com/nouser2013/lighthouse-v2-manager/releases) into the target folder from step 1. Depending on the destination, administrative privileges may be required for copying.
+1. Download and copy the files (executable and both icons) from [this repository's releases page](https://github.com/ihaian/lighthouse-v1-manager/releases) into the target folder from step 1. Depending on the destination, administrative privileges may be required for copying.
 1. Make sure your Bluetooth Low-Energy dongle is connected.
 1. Open a command prompt and navigate to the folder from step 1, e.g. `C:\Program Files\LH-Manager`. From there, follow the instructions in the chapter "Usage".
 
@@ -18,40 +21,40 @@ Make sure that you have the following:
 * bleak installed `pip3 install bleak` 
 
 ## usage with command line arguments
-The script provides three usage options: discovery, turning on and switch to standby of a lighthouse V2.
+The script provides three usage options: discovery, turning on and switch to standby of a lighthouse v1.
 
-**Note:** if you installed the binary version, simply call the program by executing it. Instead of `python3 .\lighthouse-v2-manager.py` type `lighthouse-v2-manager.exe` in the command prompt window for the commands below.
+**Note:** if you installed the binary version, simply call the program by executing it. Instead of `python3 .\lighthouse-v1-manager.py` type `lighthouse-v1-manager.exe` in the command prompt window for the commands below.
 
 ### display instructions
-If you call the executable/script with no command line arguments or with an invalid command, the useage instructions are returned. Choose one of the commands `discover`, `on` or `off` to interact with your Lighthouses V2.
+If you call the executable/script with no command line arguments or with an invalid command, the useage instructions are returned. Choose one of the commands `discover`, `on` or `off` to interact with your Lighthouses v1.
 
-**usage:** `python3 .\lighthouse-v2-manager.py`
+**usage:** `python3 .\lighthouse-v1-manager.py`
 
 ### discovery
-If you call the program with the `discover` command, it tries to open your BLE device and scans for BLE servers in range. Once found, it looks for the service and characteristic which allow for the power-up and power-down of a lighthouse V2. Look for the MAC addresses and the results on the console output.
+If you call the program with the `discover` command, it tries to open your BLE device and scans for BLE servers in range. Once found, it looks for the service and characteristic which allow for the power-up and power-down of a lighthouse v1. Look for the MAC addresses and the results on the console output.
 
 Optionally, since version 1.1, you can specify the command line option `-cs` or `--create-shortcuts` with the discovery command. The program then tries to create suitable shortcuts for your installation and your Lighthouses' MAC addresses. This works with both the script version and the binary stand-alone version.
 
-**usage:** `python3 .\lighthouse-v2-manager.py discover [-cs,--create-shortcuts]`
+**usage:** `python3 .\lighthouse-v1-manager.py discover [-cs,--create-shortcuts]`
 
 ### switch lighthouses into standby
 If you want to switch a lighthouse off ("stand-by"), specify either "off" as first argument and then each MAC address consecutively as further arguments like so:
 
-**usage:** `python3 .\lighthouse-v2-manager.py off aa:aa:aa:aa:aa:aa bb:bb:bb:bb:bb:bb ...`
+**usage:** `python3 .\lighthouse-v1-manager.py off aa:aa:aa:aa:aa:aa bb:bb:bb:bb:bb:bb ...`
 
 The lighthouses LED will now start a blue breathing animation, that is, it will fade-in and fade-out to indicate its standby operation state.
 
 ### turning lighthouses back on
 If you want to switch a lighthouse back on, specify either "on" as first argument and then each MAC address consecutively as further arguments like so:
 
-**usage:** `python3 .\lighthouse-v2-manager.py on aa:aa:aa:aa:aa:aa bb:bb:bb:bb:bb:bb ...`
+**usage:** `python3 .\lighthouse-v1-manager.py on aa:aa:aa:aa:aa:aa bb:bb:bb:bb:bb:bb ...`
 
 The lighthouses LED will power up. As soon as it's stabilized, the LED turns solid green.
 
 ### Hard-coding your Lighthouses' MAC addresses
 Inside the script, you can edit the list `lh_macs` to contain the MAC addresses of your lighthouses as strings. Doing so allows a shorter command line interaction:
-* `python3 .\lighthouse-v2-manager.py on`
-* `python3 .\lighthouse-v2-manager.py off`
+* `python3 .\lighthouse-v1-manager.py on`
+* `python3 .\lighthouse-v1-manager.py off`
 
 Still, you can add other MAC addresses dynamically even after you put some in the file itself:
-* `python3 .\lighthouse-v2-manager.py off cc:cc:cc:cc:cc:cc`
+* `python3 .\lighthouse-v1-manager.py off cc:cc:cc:cc:cc:cc`
--- lighthouse-v1-manager.py
+++ lighthouse-v1-manager.py
@@ -2,17 +2,19 @@ import asyncio
 import sys
 import re
 import os
+import re
 
-__PWR_SERVICE        = "00001523-1212-efde-1523-785feabcd124"
-__PWR_CHARACTERISTIC = "00001525-1212-efde-1523-785feabcd124"
-__PWR_ON             = bytearray([0x01])
-__PWR_STANDBY        = bytearray([0x00])
+__PWR_SERVICE		= "0000cb00-0000-1000-8000-00805f9b34fb"
+__PWR_CHARACTERISTIC = "0000cb01-0000-1000-8000-00805f9b34fb"
+# __PWR_ON			 = bytearray([0x01])
+# __PWR_STANDBY		= bytearray([0x00])
 
 command = ""
 lh_macs = [] # hard code mac addresses here if you want, otherwise specify in command line
+lh_ids = []  # hard code SN numbers here if you want, otherwise specify in command line
 
 print(" ")
-print("=== LightHouse V2 Manager ===")
+print("=== LightHouse V1 Manager ===")
 print(" ")
 
 cmdName = os.path.basename(sys.argv[0])
@@ -27,31 +29,32 @@ if len(sys.argv)>1 and sys.argv[1] in ["on", "off", "discover"]:
 if len(sys.argv)==1 or command=="":
 	print(" Invalid or no command given. Usage:")
 	print(" ")
-	print(" * discover lighthouses V2:")
+	print(" * discover lighthouses V1:")
 	print("   "+ cmdStr +" discover [--create-shortcuts, -cs]")
 	print(" ")
-	print(" * power one or more lighthoses V2 ON:")
+	print(" * power one or more lighthoses V1 ON:")
 	print("   "+ cmdStr +" on [MAC1] [MAC2] [...MACn]")
 	print(" ")
-	print(" * power one or more lighthoses V2 OFF:")
+	print(" * power one or more lighthoses V1 OFF:")
 	print("   "+ cmdStr +" off [MAC1] [MAC2] [...MACn]")
 	print(" ")
 	sys.exit()
 
 from bleak import discover, BleakClient
 
-async def run(loop, lh_macs):
+async def run(loop, lh_macs, lh_ids):
 	if command == "discover":
 		lh_macs = []
+		lh_ids = []
 		createShortcuts = True if ("-cs" in sys.argv or "--create-shortcuts" in sys.argv) else False
-		print(">> MODE: discover suitable V2 lighthouses")
-		if createShortcuts: print("         and create desktop shortcuts")
+		print(">> MODE: discover suitable V1 lighthouses")
+		if createShortcuts: print("		 and create desktop shortcuts")
 		print(" ")
 		print (">> Discovering BLE devices...")
 		devices = await discover()
 		for d in devices:
 			deviceOk = False
-			if d.name.find("LHB-") != 0:
+			if d.name.find("HTC BS") != 0:
 				continue
 			print (">> Found potential Valve Lighthouse at '"+ d.address +"' with name '"+ d.name +"'...")
 			services = None
@@ -67,63 +70,68 @@ async def run(loop, lh_macs):
 					for c in s.characteristics:
 						if c.uuid==__PWR_CHARACTERISTIC:
 							print("   OK: Characteristic "+ __PWR_CHARACTERISTIC +" found.")
-							print(">> This seems to be a valid V2 Base Station.")
+							print(">> This seems to be a valid V1 Base Station.")
 							print(" ")
 							lh_macs.append(d.address)
+							_bsid = re.search(r"HTC BS \w\w(\w\w\w\w)", str(d.name))
+							bsid = _bsid.group(1)
+							lh_ids.append(bsid)
 							deviceOk = True
 			if not deviceOk:
 				print(">> ERROR: Service or Characteristic not found.")
-				print(">>        This is likely NOT a suitable Lighthouse V2.")
+				print(">>		This is likely NOT a suitable Lighthouse V.")
 				print(" ")
 		if len(lh_macs)>0:
-			print(">> OK: At least one compatible V2 lighthouse was found.")
+			print(">> OK: At least one compatible V1 lighthouse was found.")
 			print(" ")
 			if createShortcuts:
 				print(">> Trying to create Desktop Shortcuts...")
 				import winshell
 				from win32com.client import Dispatch
 				desktop = winshell.desktop()
-				path = os.path.join(desktop, "LHv2-ON.lnk")
+				path = os.path.join(desktop, "LHv1-ON.lnk")
 				shell = Dispatch('WScript.Shell')
 				shortcut = shell.CreateShortCut(path)
 				if cmdName.find(".py")>0:
 					shortcut.Targetpath = sys.executable
-					shortcut.Arguments = '"' + cmdName + '" on '+ " ".join(lh_macs)
+					shortcut.Arguments = '"' + cmdName + '" on '+ " ".join(lh_macs) + " " + " ".join(lh_ids)
 				else:
 					shortcut.Targetpath = '"' + cmdPath + cmdName + '"'
-					shortcut.Arguments = "on "+ " ".join(lh_macs)
+					shortcut.Arguments = "on "+ " ".join(lh_macs) + " " + " ".join(lh_ids)
 				shortcut.WorkingDirectory = cmdPath[:-1]
-				shortcut.IconLocation = cmdPath + "lhv2_on.ico"
+				shortcut.IconLocation = cmdPath + "lhv1_on.ico"
 				shortcut.save()
-				print("   * OK: LHv2-ON.lnk was created successfully.")
-				path = os.path.join(desktop, "LHv2-OFF.lnk")
+				print("   * OK: LHv1-ON.lnk was created successfully.")
+				path = os.path.join(desktop, "LHv1-OFF.lnk")
 				shell = Dispatch('WScript.Shell')
 				shortcut = shell.CreateShortCut(path)
 				if cmdName.find(".py")>0:
 					shortcut.Targetpath = sys.executable
-					shortcut.Arguments = '"' + cmdName + '" off '+ " ".join(lh_macs)
+					shortcut.Arguments = '"' + cmdName + '" off '+ " ".join(lh_macs) + " " + " ".join(lh_ids)
 				else:
 					shortcut.Targetpath = '"' + cmdPath + cmdName + '"'
-					shortcut.Arguments = "off "+ " ".join(lh_macs)
+					shortcut.Arguments = "off "+ " ".join(lh_macs) + " " + " ".join(lh_ids)
 				shortcut.WorkingDirectory = cmdPath[:-1]
-				shortcut.IconLocation = cmdPath + "lhv2_off.ico"
+				shortcut.IconLocation = cmdPath + "lhv1_off.ico"
 				shortcut.save()
-				print("   * OK: LHv2-OFF.lnk was created successfully.")
+				print("   * OK: LHv1-OFF.lnk was created successfully.")
 			else:
 				print("   OK, you need to manually create two links, for example on your desktop:")
 				print(" ")
 				print("   To turn your lighthouses ON:")
-				print("    * Link Target: "+ cmdStr +" on "+ " ".join(lh_macs))
+				print("	* Link Target: "+ cmdStr +" on "+ " ".join(lh_macs)) + " " + " ".join(lh_ids)
 				print(" ")
 				print("   To turn your lighthouses OFF:")
-				print("    * Link Target: "+ cmdStr +" off "+ " ".join(lh_macs))
+				print("	* Link Target: "+ cmdStr +" off "+ " ".join(lh_macs)) + " " + " ".join(lh_ids)
 		else:
-			print(">> Sorry, not suitable V2 Lighthouses found.")
+			print(">> Sorry, not suitable V1 Lighthouses found.")
 		print(" ")
 
 	if command in ["on", "off"]:
 		print(">> MODE: switch lighthouses "+ command.upper())
-		lh_macs.extend(sys.argv[2:])
+		nums = int((len(sys.argv) / 2) - 1)		
+		lh_macs.extend(sys.argv[2:(2 + nums)])
+		lh_ids.extend(sys.argv[(2 + nums):])
 		for mac in list(lh_macs):
 			if re.match("[0-9a-fA-F]{2}(:[0-9a-fA-F]{2}){5}", mac):
 				continue
@@ -137,13 +145,30 @@ async def run(loop, lh_macs):
 		for mac in lh_macs:
 			print("   * "+mac)
 		print(" ")
-		for mac in lh_macs:
+		for i in range(len(lh_macs)):
+			mac = lh_macs[i]
+			sn = lh_ids[i]
 			print(">> Trying to connect to BLE MAC '"+ mac +"'...")
 			try:
 				client = BleakClient(mac, loop=loop)
 				await client.connect()
 				print(">> '"+ mac +"' connected...")
-				await client.write_gatt_char(__PWR_CHARACTERISTIC, __PWR_ON if command=="on" else __PWR_STANDBY)
+
+				ba = bytearray()
+				if command=="on":
+					ba += 0x1201.to_bytes(2, byteorder='big')
+					ba += 0x1202.to_bytes(2, byteorder='big')
+					ba += 0xffffffff.to_bytes(4, byteorder='little')
+					ba += (0).to_bytes(12, byteorder='big')
+				else:
+					ba += 0x1202.to_bytes(2, byteorder='big')
+					ba += (4).to_bytes(2, byteorder='big')
+					ba += int(sn, 16).to_bytes(4, byteorder='little')
+					ba += (0).to_bytes(12, byteorder='big')
+
+				print(''.join('{:02x}'.format(x) for x in ba))
+				await client.write_gatt_char(__PWR_CHARACTERISTIC, ba)  
+
 				print(">> LH switched to '"+ command +"' successfully... ")
 				await client.disconnect()
 				print(">> disconnected. ")
@@ -152,4 +177,4 @@ async def run(loop, lh_macs):
 			print(" ")
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(run(loop, lh_macs))
+loop.run_until_complete(run(loop, lh_macs, lh_ids))
